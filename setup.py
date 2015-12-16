52b2a30fe1d188bf82442248b8e988d673c0ff66
Juan Madurga <jlmadurga@gmail.com>
2015-12-16 11:57:54 +0100

    Version:0.1.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='yowsup-gateway',
-    version='0.1.0',
+    version='0.1.1',
     description="Yowsup Gateway pretends to be a link between yowsup library and client applications",
     long_description=readme + '\n\n' + history,
     author="Juan Madurga",
--- yowsup_gateway/__init__.py
+++ yowsup_gateway/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Juan Madurga'
 __email__ = 'jlmadurga@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 from yowsup_gateway.stack import YowsupGateway  # NOQA
